69cac0f2b2818ba017486d12940f14780cfb61a8
oLeVanBan <le.van.ban@sun-asterisk.com>
2020-09-30 16:20:56 +0700

    add redis

--- config/puma.rb
+++ config/puma.rb
@@ -22,6 +22,8 @@ app_dir = "/home/azureuser/azure"
 bind  "unix://#{app_dir}/tmp/sockets/puma.sock"
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
+
+daemonize true
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
